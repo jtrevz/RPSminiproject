ac363ca112f4dc34258737739b8a36e128b7a3ac
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-03-15 19:08:43 -0500

    starter code with instructions

---
+++ script.js
@@ -0,0 +1,31 @@
+var wins = 0;
+var ties = 0;
+var losses = 0;
+
+// Array of options for computer to pick from
+var options = ["R", "P", "S"];
+
+var playGame = function() {
+  // Ask user for their choice
+
+  // If user pressed Cancel, immediately end function
+
+  // Convert to uppercase to make comparisons easier
+
+  // Get random index and have computer pick based on that
+
+  // If choices are the same, it's a tie
+
+  // Check every win condition for the player
+
+  // If above conditions failed, assume player lost
+ 
+  // Print stats with line breaks
+ 
+  // Ask user to play again
+
+  // If user pressed OK, run the function again
+};
+
+// Run the game for the first time
+playGame();
